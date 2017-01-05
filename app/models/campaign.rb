4ac72f862a9daad12d0ca093084981b10f03ab2e
Adam Stevenson <adam@stripe.com>
2017-01-05 09:15:44 -0800

    change campaign max to m

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -5,7 +5,7 @@ class Campaign < ApplicationRecord
   presence: true, length: { minimum: 5, maximum: 100 }
 
   validates :goal, 
-  presence: true, numericality: { greater_than: 20, less_than: 20000 }
+  presence: true, numericality: { greater_than: 20, less_than: 1000000 }
 
   validates :description,
   presence: true, length: { minimum: 10, maximum: 5000 }
