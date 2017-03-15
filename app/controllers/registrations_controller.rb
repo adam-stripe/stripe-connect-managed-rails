034a06e07628d02846fa8940575fd7291c0c5f11
Adam Stevenson <adam@stripe.com>
2017-03-15 16:06:42 -0700

    fix redirect on signup, copy changes, etc.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
-class RegistrationsController < ApplicationController
-  def after_sign_up_path_for(user)
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(resource)
     new_campaign_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
+
   root to: "campaigns#home"
   get 'dashboard', to: 'campaigns#dashboard'
   get 'pricing', to: 'pages#pricing'
