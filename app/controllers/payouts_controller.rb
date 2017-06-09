370e8df2087c3affd704a0bae57d1e10eaf53d83
Adam Stevenson <adam@stripe.com>
2017-06-08 21:49:24 -0700

    remove redundant begin

--- app/controllers/payouts_controller.rb
+++ app/controllers/payouts_controller.rb
@@ -2,42 +2,39 @@ class PayoutsController < ApplicationController
   def show
     # Retrieve the payout from Stripe to get details
     # For large production applications, it's usually best to store this state locally
-    begin
-      if params[:id]
-        begin
-          @stripe_account = Stripe::Account.retrieve(current_user.stripe_account)
+    if params[:id]
+      begin
+        @stripe_account = Stripe::Account.retrieve(current_user.stripe_account)
 
-          # Get the payout details
-          @payout = Stripe::Payout.retrieve(
-            {
-              id: params[:id]
-            },
-            { stripe_account: current_user.stripe_account }
-          )
+        # Get the payout details
+        @payout = Stripe::Payout.retrieve(
+          {
+            id: params[:id]
+          },
+          { stripe_account: current_user.stripe_account }
+        )
 
-          # Get the balance transactions from the payout
-          @txns = Stripe::BalanceTransaction.list(
-            {
-              payout: params[:id],
-              expand: ['data.source.source_transfer'],
-              limit: 100
-            },
-            { stripe_account: current_user.stripe_account }
-          )
-        # Handle exceptions from Stripe
-        rescue Stripe::StripeError => e
-          flash[:error] = e.message
-          redirect_to dashboard_path
-        rescue => e
-          # Something else happened, completely unrelated to Stripe
-          flash[:error] = e.message
-          redirect_to dashboard_path
-        end
-      else 
-        flash[:error] = "Sorry, this payout was not found"
+        # Get the balance transactions from the payout
+        @txns = Stripe::BalanceTransaction.list(
+          {
+            payout: params[:id],
+            expand: ['data.source.source_transfer'],
+            limit: 100
+          },
+          { stripe_account: current_user.stripe_account }
+        )
+      # Handle exceptions from Stripe
+      rescue Stripe::StripeError => e
+        flash[:error] = e.message
+        redirect_to dashboard_path
+      rescue => e
+        # Something else happened, completely unrelated to Stripe
+        flash[:error] = e.message
         redirect_to dashboard_path
       end
+    else
+      flash[:error] = "Sorry, this payout was not found"
+      redirect_to dashboard_path
     end
-
   end
 end
