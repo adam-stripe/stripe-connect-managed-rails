7f988471debe3f3326a89b0ac94a5756b1e53aa9
Adam Stevenson <adam@stripe.com>
2017-10-19 14:24:41 -0700

    clean up design a bit

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 Kaminari.configure do |config|
-  config.default_per_page = 5
+  config.default_per_page = 6
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
